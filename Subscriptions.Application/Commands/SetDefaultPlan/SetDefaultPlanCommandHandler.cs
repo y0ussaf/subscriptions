eebb6e445b7a00e21445f1341c8a4232214721c8
Douirek Youssef <38112485+y0ussaf@users.noreply.github.com>
2021-03-19 11:24:30 +0000

    Set DefaultOfferForPlanCommand

---
+++ Subscriptions.Application/Commands/SetDefaultPlan/SetDefaultPlanCommandHandler.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using Subscriptions.Application.Common.Exceptions;
+using Subscriptions.Application.Common.Interfaces;
+
+namespace Subscriptions.Application.Commands.SetDefaultPlan
+{
+    public class SetDefaultPlanCommandHandler : IRequestHandler<SetDefaultPlanCommand>
+    {
+        private readonly IUnitOfWorkContext _unitOfWorkContext;
+        private readonly IPlansRepository _plansRepository;
+        private readonly IAppsRepository _appsRepository;
+
+        public SetDefaultPlanCommandHandler(IUnitOfWorkContext unitOfWorkContext,
+            IPlansRepository plansRepository,
+            IAppsRepository appsRepository
+        )
+        {
+            _unitOfWorkContext = unitOfWorkContext;
+            _plansRepository = plansRepository;
+            _appsRepository = appsRepository;
+        }
+
+        public async Task<Unit> Handle(SetDefaultPlanCommand request, CancellationToken cancellationToken)
+        {
+            await using (var unitOfWork = await _unitOfWorkContext.CreateUnitOfWork())
+            {
+                await unitOfWork.BeginWork();
+                try
+                {
+                    var app = await _appsRepository.GetApp(request.AppId);
+                    if (app is null)
+                    {
+                        throw new NotFoundException("");
+                    }
+
+                    var plan = await _plansRepository.GetPlan(request.PlanId);
+                    if (plan is null)
+                    {
+                        throw new NotFoundException("");
+                    }
+                    await _plansRepository.SetDefaultPlan(request.AppId,request.PlanId);
+                    await unitOfWork.CommitWork();
+                    return Unit.Value;
+                }
+                catch (Exception)
+                {
+                    await unitOfWork.RollBack();
+                    throw;
+                }
+            }
+        }
+    }
+}
