3e811fc40142bd4b71647869f608719cfcbca658
Douirek Youssef <38112485+y0ussaf@users.noreply.github.com>
2021-03-29 12:11:58 +0000

    pause subscription command

---
+++ Subscriptions.Application/Commands/PauseSubscription/PauseSubscriptionCommandHandler.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using Subscriptions.Application.Common.Exceptions;
+using Subscriptions.Application.Common.Interfaces;
+using Subscriptions.Domain.Entities;
+
+namespace Subscriptions.Application.Commands.PauseSubscription
+{
+    public class PauseSubscriptionCommandHandler : IRequestHandler<PauseSubscriptionCommand,PauseSubscriptionCommandResponse>
+    {
+        private readonly IUnitOfWorkContext _unitOfWorkContext;
+        private readonly ISubscriptionsRepository _subscriptionsRepository;
+
+        public PauseSubscriptionCommandHandler(IUnitOfWorkContext unitOfWorkContext,ISubscriptionsRepository subscriptionsRepository)
+        {
+            _unitOfWorkContext = unitOfWorkContext;
+            _subscriptionsRepository = subscriptionsRepository;
+        }
+
+        public async Task<PauseSubscriptionCommandResponse> Handle(PauseSubscriptionCommand request, CancellationToken cancellationToken)
+        {
+            //subscription owner or admin are allowed
+            await using (var unitOfWork = await _unitOfWorkContext.CreateUnitOfWork())
+            {
+                await unitOfWork.BeginWork();
+                try
+                {
+                    var subscription = await _subscriptionsRepository.GetSubscription(request.SubscriptionId);
+                    if (subscription is null)
+                    {
+                        throw new NotFoundException("");
+                    }
+
+                    if (!subscription.IsActive())
+                    {
+                        throw new BadRequestException("you can pause only active subscription");
+                    }
+
+                    subscription.Status = SubscriptionStatus.Paused;
+                    await _subscriptionsRepository.SetStatus(subscription);
+                    await unitOfWork.CommitWork();
+                    return new PauseSubscriptionCommandResponse();
+                }
+                catch (Exception)
+                {
+                    await unitOfWork.RollBack();
+                    throw;
+                }
+            }
+        }
+    }
+}
